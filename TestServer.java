66097abeebb5c458e516f1e35e7c5ecd46b38daa
SarthakD185 <ssd2115@rit.edu>
2021-12-02 22:19:38 -0500

    Initial commit

---
+++ TestServer.java
@@ -0,0 +1,273 @@
+import java.io.*;
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.control.Alert.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.geometry.*;
+import java.io.BufferedReader;
+import java.util.ArrayList;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+
+public class TestServer extends Application implements EventHandler<ActionEvent> {
+   // Window Attributes
+   private Stage stage;
+   private Scene scene;
+   private VBox root = null;
+
+   // GUI components
+   private TextArea taText = new TextArea();
+   private Button btnRecords = new Button("Show Records");
+   private Button btnFile = new Button("Choose File");
+   
+   // Networking
+   private static final int SERVER_PORT = 32001; 
+   private long startTime = 0;
+   private Vector<String> users = new Vector<String>();
+   private Socket socket = null;
+   private DataInputStream dis = null;
+   private DataOutputStream dos = null;
+   private Scanner scn = null; 
+   
+   private ObjectOutputStream out = null; 
+   
+   private ArrayList<String> text = new ArrayList<String>();
+    
+   /** Main program */
+   public static void main(String[] args) {
+      launch(args);
+   }
+   
+   /** start the server */
+   public void start(Stage _stage) {
+      stage = _stage;
+      stage.setTitle("Test Server Group3)");
+      final int WIDTH = 450;
+      final int HEIGHT = 400;
+      final int X = 550;
+      final int Y = 100;
+   
+      stage.setX(X);
+      stage.setY(Y);
+      stage.setOnCloseRequest(
+         new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent evt) { System.exit(0); }
+         });
+      
+      // Set up root
+      root = new VBox();
+      
+      // Put clear button in North
+      HBox hbNorth = new HBox();
+      hbNorth.setAlignment(Pos.CENTER);
+      hbNorth.getChildren().add(btnRecords);
+      hbNorth.getChildren().add(btnFile);
+      
+      // Set up rootis
+      root.getChildren().addAll(hbNorth, taText);
+      for(Node n: root.getChildren()) {
+         VBox.setMargin(n, new Insets(10));
+      }
+      
+      // Set the scene and show the stage
+      scene = new Scene(root, WIDTH, HEIGHT);
+      stage.setScene(scene);
+      stage.show();
+      
+      // handle button click
+      btnRecords.setOnAction(this);
+      btnFile.setOnAction(this);
+      
+      // Adjust size of TextArea
+      taText.setPrefHeight(HEIGHT - hbNorth.getPrefHeight());
+      
+      // do Server Stuff
+      Thread t = 
+         new Thread() {
+            public void run() {
+               doServerStuff();
+            }
+         };
+      t.start();
+   }
+   
+   /** Server action */
+   private void doServerStuff() {
+      startTime = System.currentTimeMillis();
+      try {
+         ServerSocket sSocket = new ServerSocket(SERVER_PORT);
+         while(true) {
+            Socket cSocket = sSocket.accept();
+            System.out.println("Client Connected");
+            Thread t = new ClientThread(cSocket);
+            t.start();
+         }
+      }
+      catch(Exception e) {
+         Alert alert = new Alert(AlertType.ERROR);
+         alert.setContentText(e.toString());
+         alert.setTitle("Exception");
+         alert.showAndWait();
+         System.exit(1);
+      }
+   }
+   
+   /** Button handler */
+   public void handle(ActionEvent ae) {
+   
+      String label = ((Button)ae.getSource()).getText();
+      switch(label) {
+         case "Show Records":
+            doShowRecords();
+            break;
+         case "Choose File":
+            doChooseFile();
+            break;
+         
+      
+      }
+   }
+   
+   private void doShowRecords(){
+   
+   }
+   
+   private void doChooseFile(){
+   
+      try{
+         FileChooser fileChooser = new FileChooser();
+         
+         File selectedFile = fileChooser.showOpenDialog(stage);
+         String fileName = selectedFile.getName();
+         
+         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+         
+         
+         String line = "";
+         try{
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            
+         
+            while((line = br.readLine()) != null){
+               
+               text.add(line);
+            
+               
+            }
+         
+         }
+         catch(FileNotFoundException e){
+            e.printStackTrace();
+            
+         }
+          
+         
+            
+      
+      }
+      catch(Exception e) {
+         Alert alert = new Alert(AlertType.ERROR);
+         alert.setContentText(e.toString());
+         alert.setTitle("Exception");
+         alert.showAndWait();
+         e.printStackTrace();
+         System.exit(1);
+      }
+      
+   }
+   
+   /** Class for a client thread */
+   class ClientThread extends Thread {
+      Socket cSocket = null;
+      Scanner scn = null;
+      PrintWriter pwt = null;
+      
+      /** constructor */
+      public ClientThread(Socket _cSocket) {
+         cSocket = _cSocket;
+      }
+      
+      /** Thread main */
+      public void run() {
+         try {
+         
+            String line = "";
+            try{
+               BufferedReader br = new BufferedReader(new FileReader("wordBank.txt"));
+            
+            
+               while((line = br.readLine()) != null){
+               
+                  text.add(line);
+               
+               
+               }
+            
+            }
+            catch(Exception e) {
+         Alert alert = new Alert(AlertType.ERROR);
+         alert.setContentText(e.toString());
+         alert.setTitle("Exception");
+         alert.showAndWait();
+         e.printStackTrace();
+         System.exit(1);
+      }
+            
+            ObjectOutputStream out = new ObjectOutputStream(cSocket.getOutputStream());
+            
+            out.writeObject(text);
+            out.flush();
+            
+            
+            
+              // switch
+            
+              
+         
+            cSocket.close();
+         }  
+         catch(Exception e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setContentText(e.toString());
+            alert.setTitle("Exception");
+            alert.showAndWait();
+            System.exit(1);
+         }
+      }  // run
+      
+      /** doRegister */
+      private void doRegister() {
+         if(scn.hasNextLine()) {
+            String name = scn.nextLine();
+            users.add(name);
+            Platform.runLater(
+               new Runnable() { 
+                  public void run() { taText.appendText(name + "\n");  } } );
+            pwt.println("REGISTERED\n" + name);
+            pwt.flush();
+         }  // if
+         else {
+            pwt.println("ERROR-EOF during Registration");
+            pwt.flush();
+         }  // else
+      } // doRegister
+      
+      /** doList */
+      private void doList() {
+         synchronized(users) {
+            pwt.println("TIME " + (System.currentTimeMillis() - startTime));
+            pwt.println("USERS " + users.size());
+            for(String u: users) {
+               pwt.println(u);
+            }
+            pwt.flush();
+         } // synchronized
+      } // doList
+   }
+}
---
+++ TypingTest.java
@@ -0,0 +1,212 @@
+   import javafx.application.Application;
+   import javafx.application.Platform;
+   import javafx.event.*;
+   import javafx.scene.*;
+   import javafx.scene.control.*;
+   import javafx.scene.control.Alert.*;
+   import javafx.scene.layout.*;
+   import javafx.stage.*;
+   import javafx.geometry.*;
+   import java.util.*;
+ 
+  import java.net.*;
+  import java.io.*;
+  /**
+   * TypingTet - main class of program
+   * @author  Group 3
+   * @version 2211
+  */
+  public class TypingTest extends Application implements EventHandler<ActionEvent> {
+     // GUI stuff
+     private Stage stage;  
+    private Scene scene;       
+     private VBox root = new VBox(8);
+    
+     // attributes
+     private WordProgressBar pbBar = new WordProgressBar();
+    
+     private TextArea taOutput = new TextArea();
+    
+     private TextField tfInput = new TextField();
+     private TextField tfName = new TextField();
+    private TextField tfIP = new TextField();
+    
+    private Button btnDone = new Button("Done");
+     private Button btnEnter = new Button("Enter");
+     private Button btnConnect = new Button("Connect");
+    
+     private Label lblName = new Label("Enter Your Name Here:");
+    private Label lblProgress = new Label("Your Progress:");
+     private Label lblInput = new Label("Type Sentence Here:");
+   
+     // boolean attribute
+     private boolean keepGoing = false;
+    
+     // Server attributes
+     public static final int SERVER_PORT = 32001;
+     private Socket socket = null;
+   
+     private String name = "UNKNOWN";
+     
+     
+     private ObjectInputStream input = null;
+         
+     // Main just instantiates an instance of this GUI class
+     public static void main(String[] args) {
+        launch(args);
+     }
+    
+     // Called automatically after launch sets up javaFX
+     public void start(Stage _stage) throws Exception {
+        stage = _stage;                        // save stage as an attribute
+        stage.setTitle("Typing Test");            // set the text in the title bar
+       
+        FlowPane fpTop = new FlowPane(8,8);
+        fpTop.getChildren().addAll(lblName, tfName, btnEnter, tfIP, btnConnect);
+        fpTop.setAlignment(Pos.CENTER);
+       
+        FlowPane fpMid = new FlowPane(8,8);
+        fpMid.getChildren().addAll(lblProgress, pbBar);
+        fpMid.setAlignment(Pos.CENTER);
+       
+        FlowPane fpBot = new FlowPane(8,8);
+        fpBot.getChildren().add(taOutput);
+        fpBot.setAlignment(Pos.CENTER);
+       
+        FlowPane fpBot2 = new FlowPane(8,8);
+        fpBot2.getChildren().addAll(lblInput, tfInput, btnDone);
+       fpBot2.setAlignment(Pos.CENTER);
+       
+       
+        btnEnter.setOnAction(this);
+        btnDone.setOnAction(this);
+        btnConnect.setOnAction(this);
+      
+        root.getChildren().addAll(fpTop, fpMid, fpBot, fpBot2);
+       
+        scene = new Scene(root, 400, 315);   // create scene of specified size
+                                               // with given layout
+        stage.setScene(scene);                 // associate the scene with the stage
+        stage.show();                          // display the stage (window)
+     }
+    
+     public void handle(ActionEvent evt) {
+        // Get the button that was clicked
+        Button btn = (Button)evt.getSource();
+       
+        // Switch on its name
+        switch(btn.getText()) {
+            case "Connect":
+               doConnect();
+               test();
+              break;
+           case "Enter":
+              doEnter();
+             break;
+          case "Done":
+             doDone();
+             break;
+       }
+    }
+   
+    public void doConnect() {
+       try {
+          socket = new Socket(tfIP.getText(), SERVER_PORT);
+       }
+       catch(IOException ioe) {
+          System.out.println("IO Exception: " + ioe + "\n");
+          return;
+       }
+    }
+   
+    public void doEnter() {
+       name = tfName.getText();
+      
+       btnEnter.setDisable(true);
+       tfName.setEditable(false);
+      
+       // System.out.println(name);
+    }
+   
+    public void doDone() {
+   
+    }
+    
+    public void test(){
+      try{
+         input = new ObjectInputStream(socket.getInputStream());
+      
+         ArrayList<String> text = (ArrayList<String>) input.readObject();
+         
+         for(int i = 0; i < text.size(); i++){
+         
+            taOutput.appendText(text.get(i));
+               
+         }
+         
+         taOutput.appendText("this works");
+      }
+      catch(Exception e) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setContentText(e.toString());
+            alert.setTitle("Exception");
+            alert.showAndWait();
+            System.exit(1);
+         }
+    }
+   
+    class WordProgressBar extends FlowPane implements Runnable {
+       // attributes
+       // Labels
+       private Label lblValue = new Label("0");
+       private Label lblProgress;
+      
+       // Progress Bar
+       private ProgressBar pbBar = new ProgressBar();
+      
+       //private int num;
+      
+       /* constructor */
+       public WordProgressBar() {
+         
+          StackPane spBar = new StackPane();
+         
+          spBar.getChildren().addAll(pbBar, lblValue);
+          this.getChildren().add(spBar);
+          this.setAlignment(Pos.CENTER);
+
+         
+          pbBar.setProgress(ProgressBar.INDETERMINATE_PROGRESS); 
+       }
+      
+       /* run method for progress bar */
+       public void run() {
+          for(int i = 1; (i <= 100 && keepGoing == true); i++) {
+             double time = (Math.random() * 100.0);
+             long timeLong = Math.round(time);
+            
+             try {
+                Thread.sleep(timeLong);
+             }
+             catch(InterruptedException ie) {
+                // handles exception
+             }
+            
+             final int finalI = i;
+         
+             // updates Progress Bar
+              Platform.runLater(new Runnable() {
+                public void run() {
+                   pbBar.setProgress(finalI / 100.0);
+                   lblValue.setText("" + finalI);
+                }
+                  
+             });
+          }
+          keepGoing = false;
+          //System.out.println("We are running " + num);
+          System.currentTimeMillis();
+       }
+    }
+ }
+
